f9d64711b8286c738398329715fbf47b718baa29
Livia Lucas <livia@buildgc.com>
2017-05-14 20:40:17 -0700

    every time the page is loaded, the console logs random values for two dices

---
+++ diceRandomValues/app.js
@@ -0,0 +1,39 @@
+/*
+
+Creating a page where every time the page is loaded, the console logs random values for two dice. 
+
+1) Write down pseudocode for the program described below.
+
+2) Follow the steps in your pseudocode to write the code for your function.
+* generate a random number between 1 - 6 and store to a variable, firstDie
+* generate a random number between 1 - 6 and store to a variable, secondDie
+* log the values of the first and second dice to the console, labeling each value
+* log the total of the two dice to the console, with a label
+
+3) Create a statement that runs the function you created whenever the page loads.
+  (Hint: Reference when the page loads with the code window.onload, and set its value to the name of the function you created, without parentheses.)
+
+*/
+
+
+function randomDiceNumber() {
+	var firstDie = Math.floor((Math.random() * 6) + 1) ,
+		secondDie = Math.floor((Math.random() * 6) + 1);
+
+	console.log(firstDie);
+	console.log(secondDie);
+	console.log((firstDie) + (secondDie));
+}
+
+window.onload = randomDiceNumber;
+
+
+function randomDiceNumberAux(firstDie, secondDie) {
+	console.log(firstDie);
+	console.log(secondDie);
+	console.log((firstDie) + (secondDie));
+
+}
+
+var firstDie = Math.floor((Math.random() * 5) + 1);
+var secondDie = Math.floor((Math.random() * 6) + 1);
